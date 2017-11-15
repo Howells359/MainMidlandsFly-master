1b06f23a15959beed59465c559dd4bd95c781611
Sana <Sana@DESKTOP-PJC0VPT>
2017-11-15 16:11:21 +0000

    Sana
    Added files of Job Scheduler

---
+++ MainMidlandsFly/AllocateGroundCrewJob.cs
@@ -0,0 +1,19 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainMidlandsFly
+{
+    public class AllocateGroundCrewJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            
+
+
+        }
+
+    }
+}
---
+++ MainMidlandsFly/AllocationScheduler.cs
@@ -0,0 +1,34 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainMidlandsFly
+{
+    public class AllocationScheduler
+    {
+        public static void Start()
+        {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<AllocateGroundCrewJob>().Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithDailyTimeIntervalSchedule
+                  (s =>
+                     s.WithIntervalInHours(24)
+                    //s.WithIntervalInSeconds(20)
+                    .OnEveryDay()
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(0, 0))
+                  )
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+
+
+    }
+}
