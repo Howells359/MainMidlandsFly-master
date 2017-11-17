3741e426b4bb72d903eaa54893460ea3897f6905
kmccor <kmccor2265@hotmail.com>
2017-11-17 15:24:56 +0000

    Added additional EmployeeID fieldin Crew table to convert CrewID integer into 5 chr string with predeceding zeros, as per assignment brief. Added mobile number and e-mail validation.

--- MainMidlandsFly/Models/Crew.cs
+++ MainMidlandsFly/Models/Crew.cs
@@ -9,20 +9,27 @@ namespace MainMidlandsFly.Models
 {
     public class Crew
     {
-        [Display(Name = "Employee ID")]
+        [Display(Name = "Crew ID")]
         [Required]
         public int CrewId { get; set; }
+
+        [Display(Name = "Employee ID")]
+        [Required]
+        public string EmployeeID { get; set; }
 
         [Display(Name = "Name")]
         [Required]
         public string Name { get; set; }
 
         [Display(Name = "Mobile No")]
-        [Required]
+        //[DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "UK Mobile Number Required!")]
+        //[RegularExpression(@"^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$", ErrorMessage = "UK mobile number format required - 07xxx xxxxxx or  (07xxx) xxxxxx or +44 7xxx xxx xxx")]
         public string MobNo { get; set; }
 
         [Display(Name = "E-Mail")]
         [Required]
+        //[DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [StringLength(100)]
