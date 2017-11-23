62c1a20ebe009c65e34349ca825fb2345cbb9e89
kmccor <kmccor2265@hotmail.com>
2017-11-23 18:31:59 +0000

    Added detailed validation to Crew model, added postcode lookup functionaility...currently not writing values to DB

--- MainMidlandsFly/Controllers/CrewController.cs
+++ MainMidlandsFly/Controllers/CrewController.cs
@@ -6,6 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MainMidlandsFly.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace MainMidlandsFly.Controllers
 {
@@ -45,16 +50,106 @@ namespace MainMidlandsFly.Controllers
         // GET: Crew/Create
         public IActionResult Create()
         {
-            return View();
+            ////create Address object to store form values, may not need to create the object here??
+            var addressIndex = new Crew();
+            ////Message string to be used by Index View
+            string msg = "Please input house number/name and post code.";
+            ViewBag.msg = msg;
+            ////again not sure if addressIndex required here as object is empty??
+            return View(addressIndex);
+            //return View();
         }
+
+
+
+        //POST Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(string postcode, string HouseNo, string Name, string Type, string Email, string MobNo, DateTime DateOfBirth)
+            {
+                //Use plugin Net.Http to connect to API, create new object
+                using (HttpClient client = new HttpClient())
+                {
+                    //Create new object to store form values in Address model
+                    var FullAddress = new Crew();
+
+                    //Store any populated form values in FullAddress Address model object
+                    if (Name != null) { FullAddress.Name = Name; }
+                    if (Type != null) { FullAddress.Type = Type; }
+                    if (Email != null) { FullAddress.Email = Email; }
+                    if (MobNo != null) { FullAddress.MobNo = MobNo; }
+                    if (DateOfBirth != null) { FullAddress.MobNo = MobNo; }
+                
+
+
+                //Connection details for GetAddress.io API that returns full addresses when House No/Name & Postcode provided
+                string apiKey = "xmZfShMoG0aWHJRPc-FkSQ11213";
+                    string website = "https://api.getaddress.io/find/" + postcode + "/" + HouseNo + "?api-key=" + apiKey;
+
+
+                    client.BaseAddress = new Uri(website);
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage response = client.GetAsync("").Result;
+
+                    //If API successfully returns address run following if
+                    if (response.IsSuccessStatusCode)
+                    {
+                        //create object with API JSON response value
+                        var AddressResponse = response.Content.ReadAsStringAsync().Result;
+
+                        //Use Newtonsoft JSON plugin to deserialize response
+                        var address1 = JsonConvert.DeserializeObject<Crew>(AddressResponse);
+
+                        //string AddressLatitude = address.latitude;
+
+                        //GetCrew() API always returns address values as an array with each address line being comma spearated values.
+                        //As we're only looking for singular addresses we always use the first and only value in the return array and assign it to a string.
+                        string fullAddress = address1.addresses[0];
+
+                        //Separate address string into an array using comma separator to identify values, removes any white space.
+                        string[] values = fullAddress.Split(',').Select(sValue => sValue.Trim()).ToArray();
+
+                        //Assign array values to associated strings
+                        string AddressLine1 = values[0];
+                        string AddressLine2 = values[1];
+                        string AddressLine3 = values[2];
+                        string AddressLine4 = values[3];
+                        string Locality = values[4];
+                        string TownOrCity = values[5];
+                        string County = values[6];
+
+                        //create array of the strings
+                        string[] myStrings = new string[] { AddressLine1, AddressLine2, AddressLine3, AddressLine4, Locality, TownOrCity, County, postcode, null };
+
+                        //Build full address string from populated (not null) values adding line break between each line 
+                        address1.formattedAddress = string.Join(System.Environment.NewLine, myStrings.Where(str => !string.IsNullOrEmpty(str)));
+
+
+                        if (address1.formattedAddress != null)
+                        {
+                        //store address value in formattedAddress model object
+                        //FullAddress.formattedAddress = address1.formattedAddress;
+                        FullAddress.Address = address1.formattedAddress;
+                        return View(FullAddress);
+                        }
+                    }
+                    //If address not found send following message to View
+                    string msg = "Address not found, please try again.";
+                    ViewBag.msg = msg;
+                    return View(FullAddress);
+                }
+            }
+
 
         // POST: Crew/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CrewId,Name,MobNo,Email,Address,DateOfBirth,Type,Status")] Crew crew)
+        public async Task<IActionResult> CreateUpdateDB([Bind("CrewId,Name,MobNo,Email,Address,DateOfBirth,Type,Status")] Crew crew)
         {
+            //Code below writes form values to SQL DB
             if (ModelState.IsValid)
             {
                 _context.Add(crew);
@@ -64,7 +159,8 @@ namespace MainMidlandsFly.Controllers
                 _context.Update(crew);
                 return RedirectToAction(nameof(Index));
             }
-            return View(crew);
+            //return View(crew);
+            return RedirectToAction("Create", crew);
         }
 
         // GET: Crew/Edit/5
---
+++ MainMidlandsFly/Models/HomeAddress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainMidlandsFly.Models
+{
+    public class HomeAddress
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        public string AddressLine4 { get; set; }
+        public string Locality { get; set; }
+        public string TownOrCity { get; set; }
+        public string County { get; set; }
+        public string PostCode { get; set; }
+    }
+}
